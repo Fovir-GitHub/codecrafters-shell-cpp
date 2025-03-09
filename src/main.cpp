8ed4b7476730e9e50f50f5c85b60d314a42a460f
Fovir <fovir@disroot.org>
2025-03-09 19:26:13 +0800

    refactor: Remove the `getCommandAndArguments()` function

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,6 @@
 #include <string>
 #include <vector>
 
-void getCommandAndArguments(std::string &              input,
-                            std::vector<std::string> & caa);
-
 int main()
 {
     // Flush after every std::cout / std:cerr
@@ -20,24 +17,9 @@ int main()
         std::string              input;
         std::vector<std::string> command_and_arguments;
         std::getline(std::cin, input);
-        getCommandAndArguments(input, command_and_arguments);
-        if (command_and_arguments[0] == "exit")
-            std::exit(command_and_arguments[1][0] - '0');
 
         std::cout << input << ": command not found" << '\n';
     }
 
     return 0;
-}
-
-void getCommandAndArguments(std::string & input, std::vector<std::string> & caa)
-{
-    caa.clear();
-
-    std::string        temp("");
-    std::istringstream iss(input);
-
-    while (iss >> temp) caa.push_back(temp);
-
-    return;
 }
