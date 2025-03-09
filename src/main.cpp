b57f37a066640746110e2db6cd077291bf0bbbc2
Fovir <fovir@disroot.org>
2025-03-09 19:06:55 +0800

    feat: Add `exit` command

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
+#include <cstdlib>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+void getCommandAndArguments(std::string &              input,
+                            std::vector<std::string> & caa);
 
 int main()
 {
@@ -10,11 +17,27 @@ int main()
     {
         std::cout << "$ ";
 
-        std::string input;
+        std::string              input;
+        std::vector<std::string> command_and_arguments;
         std::getline(std::cin, input);
+        getCommandAndArguments(input, command_and_arguments);
+        if (command_and_arguments[0] == "exit")
+            std::exit(command_and_arguments[1][0] - '0');
 
         std::cout << input << ": command not found" << '\n';
     }
 
     return 0;
+}
+
+void getCommandAndArguments(std::string & input, std::vector<std::string> & caa)
+{
+    caa.clear();
+
+    std::string        temp("");
+    std::istringstream iss(input);
+
+    while (iss >> temp) caa.push_back(temp);
+
+    return;
 }
