3e2862fb0bf6f2a0467dc0153274e1e1b5300026
Fovir <fovir@disroot.org>
2025-03-19 15:38:54 +0800

    feat: add `FindPossibleStringByPrefix()` method for `Trie` class

--- src/trie.cpp
+++ src/trie.cpp
@@ -1,10 +1,33 @@
 #include "trie.h"
 
+void Trie::FindPossibleStringByPrefixHelper(
+    std::shared_ptr<TrieNode> & current_node, std::vector<std::string> & result,
+    std::string & prefix)
+{
+    for (auto & [next_character, next_node] : current_node->children)
+    {
+        // There is no next node
+        if (!next_node)
+            continue;
+
+        // Add the next character to the prefix string
+        prefix.push_back(next_character);
+        if (next_node->is_end) /* If the next node is one end of a word */
+            result.push_back(prefix); /* Add it to the result */
+
+        // Recrusively find the possible string
+        FindPossibleStringByPrefixHelper(next_node, result, prefix);
+        prefix.pop_back(); /* Step back */
+    }
+
+    return;
+}
+
 void Trie::Insert(const std::string & word)
 {
-    auto node = root;
+    std::shared_ptr<TrieNode> node = root;
 
-    for (const auto & ch : word)
+    for (const char & ch : word)
     {
         if (!node->children[ch])
             node->children[ch] = std::make_shared<TrieNode>();
@@ -15,4 +38,27 @@ void Trie::Insert(const std::string & word)
     node->is_end = true;
 
     return;
+}
+
+std::vector<std::string> Trie::FindPossibleStringByPrefix(std::string & prefix)
+{
+    std::shared_ptr<TrieNode> node = root;
+    std::vector<std::string>  result;
+
+    // Find the prefix at first
+    for (const char & ch : prefix)
+    {
+        // Not found
+        if (!node->children[ch])
+            return result; /* Return empty result */
+        node = node->children[ch];
+    }
+
+    if (node->is_end) /* The prefix is a possible word */
+        result.push_back(prefix);
+
+    // Find the remaining possible strings
+    FindPossibleStringByPrefixHelper(node, result, prefix);
+
+    return result;
 }
--- src/trie.h
+++ src/trie.h
@@ -5,7 +5,6 @@
 #include <unordered_map>
 #include <vector>
 
-
 class Trie
 {
 private:
@@ -21,13 +20,31 @@ private:
 
     std::shared_ptr<TrieNode> root;
 
+    /**
+     *@brief Help to find the possible string by prefix by dfs method
+     *
+     * @param current_node the current TrieNode
+     * @param result the array of result
+     * @param prefix the prefix
+     */
+    void
+    FindPossibleStringByPrefixHelper(std::shared_ptr<TrieNode> & current_node,
+                                     std::vector<std::string> &  result,
+                                     std::string &               prefix);
+
 public:
     Trie() : root(std::make_shared<TrieNode>()) {}
+    ~Trie() {}
 
     void Insert(const std::string & word);
 
-    std::vector<std::string>
-    FindPossibleStringByPrefix(const std::string & prefix);
+    /**
+     *@brief Find the possible string with the prefix
+     *
+     * @param prefix
+     * @return std::vector<std::string> all possible strings
+     */
+    std::vector<std::string> FindPossibleStringByPrefix(std::string & prefix);
 };
 
 #endif // !_TRIE_H_
