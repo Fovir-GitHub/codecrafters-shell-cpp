d76db29211d763e9fa902e385dc0af896699ffce
Fovir <fovir@disroot.org>
2025-03-20 23:50:49 +0800

    feat: add `removeQuoteSigns()` function

---
+++ src/tools.cpp
@@ -0,0 +1,19 @@
+#include "tools.h"
+
+std::string removeQuoteSigns(std::string s)
+{
+    /**
+     * If the length of the string is less than 2
+     * or the first and last characters are not the same,
+     * then return the original string
+     */
+    if (s.size() < 2 || s.front() != s.back())
+        return s;
+
+    /**
+     * If the first character is quote sign, then return the substring
+     * Otherwise, return the original string
+     */
+    return (s.front() == '\"' || s.front() == '\'') ? s.substr(1, s.size() - 2)
+                                                    : s;
+}
---
+++ src/tools.h
@@ -0,0 +1,14 @@
+#ifndef _TOOLS_H_
+#define _TOOLS_H_
+
+#include <string>
+
+/**
+ *@brief Remove the quote signs around the string
+ *
+ * @param s the string to operate
+ * @return std::string the result string without quote signs
+ */
+std::string removeQuoteSigns(std::string s);
+
+#endif // !_TOOLS_H_
